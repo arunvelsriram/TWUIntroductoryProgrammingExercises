87c1f702b7f9a366f86d5deb38f5fedec462af5e
Arunvel Sriram <arunvelsriram@gmail.com>
2015-05-29 23:27:23 +0530

    Finish triangle exercises

--- src/com/thoughtworks/main/Main.java
+++ src/com/thoughtworks/main/Main.java
@@ -2,7 +2,33 @@ package com.thoughtworks.main;
 
 public class Main {
 
+    public void printSingleAsterisk() {
+        System.out.println("*");
+    }
+
+    public void printHorizontalLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public void printVerticalLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+
+        main.printSingleAsterisk();
+        main.printHorizontalLine(5);
+        main.printVerticalLine(5);
+//        main.printRightTriangle(5);
+//        main.printIsoscelesTriangle(5);
+//        main.printDiamond(5);
+//        main.printDiamondWithName(3, "arun");
+//        main.fizzBuzz();
+//        System.out.println(main.generate(625));
     }
 }
